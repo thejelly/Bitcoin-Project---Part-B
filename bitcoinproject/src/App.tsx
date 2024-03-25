94acbe5e5b207c20d428fb8f1b4cfaeb925d62ce
mayou7mi <xiong.chu@northeastern.edu>
2024-03-25 16:08:53 -0400

    rebuild project in typescript

---
+++ bitcoinproject/src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <a>23</a>
+    </div>
+  );
+}
+
+export default App;
